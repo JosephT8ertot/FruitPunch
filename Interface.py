dd66f5ecc617fdf2edac935197601322d631202c
JosephT8ertot <josepht8ertot@gmail.com>
2021-06-26 21:30:38 -0400

    Added a good start to the Interface. Need to finish league createGames and find a way to pass information upon click.

--- Interface.py
+++ Interface.py
@@ -9,14 +9,62 @@ curious.
 
 # imports
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 # Home screen
 class Home(Screen):
     pass
 
-# Screen Manager
+# Login screen
+class Login(Screen):
+    pass
+
+# CreateUser Screen
+class CreateUser(Screen):
+    pass
+
+# Dashboard screen
+class Dashboard(Screen):
+    pass
+
+# Group screen
+class Group(Screen):
+    pass
+
+# AdminGroup screen
+class AdminGroup(Screen):
+    pass
+
+# BeerPong screen
+class BeerPong(Screen):
+    pass
+
+# Snappa screen
+class Snappa(Screen):
+    pass
+
+# BPTournament screen
+class BeerPongTournament(Screen):
+    pass
+
+# SnappaTournament screen
+class SnappaTournament(Screen):
+    pass
+
+# BPLeague screen
+class BeerPongLeague(Screen):
+    pass
+
+# SnappaLeague screen
+class SnappaLeague(Screen):
+    pass
+
+# Team screen
+class Team(Screen):
+    pass
+
+# WindowManager Class
 class WindowManager(ScreenManager):
     pass
 
@@ -24,3 +72,7 @@ class WindowManager(ScreenManager):
 class Interface(App):
     def build(self):
         pass
+
+# Kivy environment variables
+Window.minimum_height = 600
+Window.minimum_width = 900
